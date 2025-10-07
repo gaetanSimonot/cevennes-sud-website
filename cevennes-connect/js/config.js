7ee6fc68e7dcf899de2315ccf0eb65fcd067129c
gaetanSimonot <83704565+gaetanSimonot@users.noreply.github.com>
2025-10-07 22:31:23 +0200

    Add config.js

---
+++ cevennes-connect/js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+      GOOGLE_MAPS_API_KEY: 'AIzaSyCSJRp7NCeKSPiKnezVyJiJFg5dqhbWnyw',
+      GOOGLE_PLACES_API_KEY: 'AIzaSyBe9S2a8Afc67NtS9UmvEwOoLt3BFne0eI',
+      GITHUB_TOKEN: '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',
+      GITHUB_REPO: 'gaetanSimonot/cevennes-sud-website',
+      FILE_PATH: 'cevennes-connect/data/actors-data.json'
+  };
